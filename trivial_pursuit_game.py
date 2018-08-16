46d12b39d37e1bd1912a0c197c9b04b8535f005f
jessica.grimes@my.jcu.edu.au <Rotom2104>
2018-08-16 13:53:33 +1000

    Reading questions in from file

--- trivial_pursuit_game.py
+++ trivial_pursuit_game.py
@@ -1,12 +1,43 @@
+from random import shuffle
+
+
 def main():
+    questions = load_questions()
+    for question in questions:
+        shuffle_options = shuffle_questions(question)
+        ask_question(question[0], shuffle_options)
 
+
+def ask_question(question, answers):
+    """Display questions to the user"""
+    print("{}?\nA) {}\nB) {}\nC)".format(question, answers[0], answers[1], answers[2]))
+    user_answer = ""
+    while user_answer != "A" and user_answer != "B" and user_answer != "C":
+        user_answer = input(">>> ")
+        user_answer = user_answer.upper()
+
+    return user_answer
+
+
+def load_questions():
+    """Loading questions reading from a file"""
+    questions = []
     question_file = open("Questions", "r")
-    questions = [ ]
-
-    for line in question_file.readlines():
+    for line in question_file:
         question = line.strip("\n").split(",")
         questions.append(question)
+    question_file.close()
 
-    print(questions)
+    return questions
+
+
+def shuffle_questions(question_row):
+    """Shuffling questions"""
+    options = []
+    for i in range(1, 4):
+        options.append(question_row[i])
+    shuffle(options)
+    return options
+
 
 main()
